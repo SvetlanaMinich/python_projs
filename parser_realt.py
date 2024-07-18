aaefc61b2d66ea2e87e61532671e81afd48a0d66
Svetlana Minich <imsvetam@mail.ru>
2024-07-18 23:26:50 +0300

    tried with realt.by but had problems with bs and celenium. restart with another site

--- parser_realt.py
+++ parser_realt.py
@@ -4,28 +4,47 @@ import pandas as pd
 import re
 import html
 
-price_usd_to = 400 
+class Scraping:
+    def __init__(self, price):
+        self._price_to = price
 
-realt = f"https://realt.by/rent/flat-for-long/?addressV2=%5B%7B%22townUuid%22%3A%224cb07174-7b00-11eb-8943-0cc47adabd66%22%7D%5D&page=1&priceTo={price_usd_to}&priceType=840"
-response = get(realt)
-print(response.status_code)
+    def url(self):
+        return 'https://weekend.by/minsk/arenda-kvartir-na-dlitelniy-srok?sort=price'   #another site
 
-html_soup = BeautifulSoup(response.text, 'html.parser')
+    @property
+    def price_to(self):
+        return self._price_to
 
+    @price_to.setter
+    def change_price_to(self, new_price):
+        if new_price > 0:
+            self._price_to = new_price
 
-houses_number = html_soup.find('div', class_ = 'justify-between items-center -mx-0 sm:-mx-0 flex flex-wrap md:mb-4 mb-2 !-mr-0.5')
-houses_number = houses_number.find('div', class_ = 'flex')
-houses_number = int(houses_number.find('b').get_text())
+    def get_soup(self):
+        url = self.url()
+        response = get(url)
+        if response.status_code == 200:
+            html_soup = BeautifulSoup(response.text, 'html.parser')
+            titles = ['price_usd', 'price_byn', 'address', 'rooms_num', 'publication_date']
+            df = pd.DataFrame(columns=titles)
+            houses = html_soup.find('div', class_ = 'posts-list').find_all('div', class_ = 'col-12d')
+            for house in houses:
+                rooms_num = house.find('div', class_ = 'related_title').find('a').text
+                rooms_num = rooms_num.split(',')[0]
 
-house_container = html_soup.find('div', {'data-index': '0'}).find('div', class_ = 'p-0 bg-white block drop-shadow-none border-basic-200 rounded-md flex flex-col md:flex-row bg-white h-full relative md:h-[374px]')
+                price_byn = house.find('div', class_ = 'price').find('span', class_ = 'pia_link').find('span').text
 
-house_url = 'https://realt.by' + house_container.find('a').get('href')
+                price_usd = house.find('div', class_ = 'price').find('span', class_ = 'doppric').text
 
-house_container = house_container.find('div', class_ = 'flex flex-col w-full h-full p-5')
+                address = house.find('div', class_ = 'info').find('div', class_ = 'probegla').text
 
-price_usd = house_container.find('span', class_ = 'text-basic text-subhead').text
-rooms_num = house_container.find('p', class_ = 'flex flex-wrap text-headline items-center font-semibold md:font-bold -mr-2 mb-2.5 md:mb-4 -order-2 md:-order-none').find('span').text
-address = house_container.find_all('div', class_ = 'w-full')[1].find('p', class_ = 'text-basic w-full text-subhead md:text-body').text
-subway_station = house_container.find('div', class_ = 'text-basic leading-none mb-2.5 md:mb-4 mt-1 md:mt-0.5 empty:mt-0 children:md:text-body children:text-subhead').find('span', class_ = "mr-4").text
-
-print(house_url)
+                appending_row = [price_usd, price_byn, address, rooms_num]
+                df_len = len(df)
+                df.loc[df_len] = appending_row
+            return df
+        else:
+            return False    
+        
+newScrap = Scraping(400)
+df = newScrap.get_soup()
+print(df)
